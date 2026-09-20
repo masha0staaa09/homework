faad74aa752b5fb2a6d9e93ab7e90669377a6c9e
masha0staaa09 <staaa.masha0@gmail.com>
2026-09-20 11:54:34 +0300

    обавлены новые функции для словарей

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """Функция принимает список словарей и возвращает отсортированный список"""
+    """Функция принимает список словарей и возвращает отсортированный список по ключу"""
     result = []
 
     for item in data:
@@ -7,3 +7,8 @@ def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
             result.append(item)
 
     return result
+
+
+def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
+    """Функция принимает список словарей и возвращает отсортированный список по дате"""
+    return sorted(data, key=lambda item: item["date"], reverse=descending)
